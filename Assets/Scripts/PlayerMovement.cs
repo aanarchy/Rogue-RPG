87506e3f932efd9de3a9dc3c4d195bcd0ada6c34
BuildTools <unconfigured@null.spigotmc.org>
2019-08-22 17:43:42 +0800

    Commit to github

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    public Animator a;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        a.SetFloat("Horizontal", movement.x);
+        a.SetFloat("Vertical", movement.y);
+        a.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/RoomTransfer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransfer : MonoBehaviour
+{
+    public Vector2 cameraMin;
+    public Vector2 cameraMax;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>(); 
+    }
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {    
+        if (other.CompareTag("Player"))
+        {
+            cam.minPosition += cameraMin;
+            cam.maxPosition += cameraMax;
+            other.transform.position += playerChange;
+        }
+    }
+} 
